dc312d3a41ee9d10db0884e18fba64ccb44c203c
rishabh-malik <rishabhmalik249@gmail.com>
2017-10-22 18:07:33 +0530

    cards dynamically

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -28,3 +28,36 @@ function closeCreatePostModal () {
 shareImageButton.addEventListener('click', openCreatePostModal)
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal)
+
+//creating cards dynamically
+function createCard(){
+  var cardWrapper=document.createElement('div');
+  cardWrapper.className='shared-moment-card mdl-card-mdl-shadow--2dp';
+  var cardTitle=document.createElement('div');
+  cardTitle.className='mdl-card__title';
+  cardTitle.style.backgroundImage='/src/images/main-image-lg.jpg';
+  cardTitle.style.backgroundSize='cover';
+  cardTitle.style.height='180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement=document.createElement('h2');
+  cardTitleTextElement.className='mdl-card__title-text';
+  cardTitleTextElement.textContent='Sans Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText=document.createElement('div');
+  cardSupportingText.className='mdl-card__supporting-text';
+  cardSupportingText.textContent='In Sans Francisco';
+  cardSupportingText.style.textAlign='center';
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  //sharedMomentsArea.appendChild(cardWrapper);
+  document.getElementById("card").appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+.then(function(res){
+  return res.json();
+})
+.then(function(data){
+  console.log('creating card');
+  createCard();
+});
