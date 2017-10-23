3f0464623e1e74ef7c2e0042c2c6283890cccb28
rishabh-malik <rishabhmalik249@gmail.com>
2017-10-23 17:53:31 +0530

    fetching all major items

--- public/sw.js
+++ public/sw.js
@@ -6,7 +6,22 @@ self.addEventListener('install',function(event){
         caches.open('static')
         .then(function(cache){
             console.log('Precaching app shell');
-            cache.add('/src/js/app.js');
+            cache.addAll([
+                '/',
+                '/src/js/app.js',
+                '/src/js/feed.js',
+                '/src/css/app.css',
+                '/src/css/feed.css',
+                '/index.html',
+                '/src/js/material.min.js',
+                '/src/images/main-image.jpg',
+                'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                "https://fonts.googleapis.com/icon?family=Material+Icons",
+                "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+            ]);
+            //cache.add('/');
+            //cache.add('/src/js/app.js');
+            //cache.add('/sindex.html');
         })
 );
 });
